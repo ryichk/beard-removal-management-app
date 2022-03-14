de4c8945ed83e8a5fd409a3872ee40d04da441b3
ryichk <ryichk@gmail.com>
2022-03-14 09:36:47 +0900

    :bug: Fixed to change redirect URLs locally and in production.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,33 @@
 import 'tailwindcss/tailwind.css';
 import { Amplify } from 'aws-amplify';
 import type { AppProps } from 'next/app';
+
 import awsconfig from '../aws-exports';
 
-Amplify.configure(awsconfig);
+if (typeof window !== 'undefined') {
+  const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
+  );
+  const [localRedirectSignIn, productionRedirectSignIn] = awsconfig.oauth.redirectSignIn.split(',');
+
+  const [localRedirectSignOut, productionRedirectSignOut] =
+    awsconfig.oauth.redirectSignOut.split(',');
+
+  const updatedAwsConfig = {
+    ...awsconfig,
+    oauth: {
+      ...awsconfig.oauth,
+      redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+      redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+    },
+  };
+
+  Amplify.configure(updatedAwsConfig);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
